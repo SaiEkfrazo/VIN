e5581953ae59d04a48ef9c9e49d2c9e6018ae9f3
SaiEkfrazo <saithimma@ekfrazo.in>
2024-05-08 17:35:28 +0530

    pushing the settings file

--- aivolved/settings.py
+++ aivolved/settings.py
@@ -83,24 +83,24 @@ AUTH_USER_MODEL = 'dashboard.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-       'default':
-           {
-           'ENGINE': 'django.db.backends.mysql',
-           'NAME': 'vin',
-           'USER': 'root',  
-           'PASSWORD': 'sai',  
-           'HOST': 'db',  
-           'PORT': '3306',    
-           }
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#        'default':
+#            {
+#            'ENGINE': 'django.db.backends.mysql',
+#            'NAME': 'vin',
+#            'USER': 'root',  
+#            'PASSWORD': 'sai',  
+#            'HOST': 'db',  
+#            'PORT': '3306',    
+#            }
+#    }
 
 
 # Password validation
@@ -134,11 +134,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/app/media'
-
 # MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = '/app/media'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 PORT = '8000'
 # Static files (CSS, JavaScript, Images)
